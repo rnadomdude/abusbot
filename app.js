01b87fd628443cd978e92baa397abab12eae66a4
rnadomdude <87897660+rnadomdude@users.noreply.github.com>
2022-05-05 11:09:54 +0800

    balls?
    !!!

---
+++ app.js
@@ -0,0 +1,23 @@
+let Discord = require("discord.js")
+let client = new Discord.Client()
+
+client.on("message", message => {
+  if(message.content === "abel") {
+    message.channel.send("Tomboys are just one of the best fucking things that has ever been introduced on this planet. They have adorable short hair, got abs, huge fat milkers and many things a girl would have but acts as your best friend. You can snuggle with one, get a blowjob, kiss, have kids, watch tv, play sports and many other things that give you joy in life. You don’t ever have to worry about spending useless hours waiting for your significant other to finish shopping at the mall only to spend $300 on a pair of boots and makeup. Any time you want they can go from shy introvert gamer to a Dommy Mommy in bed, Shy in the streets, kinky in the sheets. They are extremely attractive having abs, a thicc body, and running with sports bra and their adorable short hair that could even turn a gay man straight. People don’t understand how hot that is, breeding with one is a dream and in the end we all want to marry one to live a happy life. People think having a tomboy gf is weird but in reality, they just haven't realized the dream until they had a chance to experience it.")
+  }
+  if(message.content === "abus") {
+    let embed = new Discord.MessageEmbed().setTitle("BALLS").setDescription("Your mom").setColor("RANDOM").setFooter("angery")
+    message.channel.send(embed)
+  }
+  if(message.content === "balls") {
+    message.channel.send("https://cdn.discordapp.com/attachments/285880764493725696/971289000298049576/SPOILER_kablooeh.mp4")
+  }
+  if(message.content === "ballss") {
+    message.channel.send("What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little clever comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.")
+  }
+})
+
+client.on
+
+client.login("OTcxMzAzMjk3MzY3NTU2MTA2.YnIisA.1gBQjydPIaqtC3g_t8D0j35C1lM")
+
